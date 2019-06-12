0d40c08b7363d345426e4d588c8d7ee3498a0b02
Unknown <helpu1409@gmail.com>
2019-06-12 13:17:19 -0500

    Modificando el main
    Se hace uso de la clase HelloWorld

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-#include <stdlib>
+#include "hello_world.h"
+
+using namespace std;
 
 int main() {
     cout << "Ensayo 1" << endl;
+    HelloWorld hello1;
+    hello1.printHello();
+
     return 0;
 }
 
